3e65dd05eb7dfc239da2ef2f44b241a0cdc0be96
Shofikulislam <37293202+Shofikulislam@users.noreply.github.com>
2019-04-08 10:11:41 +0600

    mopo

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+    	$(".video-play-btn").magnificPopup({
+    		type: 'video',
+    	});
+    		
+    		
+    });
+
+
+
+}(jQuery));	
